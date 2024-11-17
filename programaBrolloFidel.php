d9ce9d08338a2de87d947b37e65de6efe75f9a3e
Fabribrollo <fabrizio.brollo@est.fi.uncoma.edu.ar>
2024-11-16 22:34:54 -0300

    coleccion partidas

--- programaBrolloFidel.php
+++ programaBrolloFidel.php
@@ -41,8 +41,8 @@ function cargarPartidas(){
     $coleccionPartidas[5] = ["palabraWordix"=> "MELON","jugador" =>"sofia", "intentos" => 2 , "puntaje" => 5];
     $coleccionPartidas[6] = ["palabraWordix"=> "FUEGO","jugador" =>"romi", "intentos" => 6 , "puntaje" => 0];
     $coleccionPartidas[7] = ["palabraWordix"=> "FUEGO","jugador" =>"juli", "intentos" => 6 , "puntaje" => 0];
-    $coleccionPartidas[8] = ["palabraWordix"=> "FUEGO","jugador" =>"rudolf", "intentos" => 0 , "puntaje" => 0];
-    $coleccionPartidas[9] = ["palabraWordix"=> "MUJER","jugador" =>"majo", "intentos" => 5 , "puntaje" => 11];
+    $coleccionPartidas[8] = ["palabraWordix"=> "FUEGO","jugador" =>"juan", "intentos" => 0 , "puntaje" => 0];
+    $coleccionPartidas[9] = ["palabraWordix"=> "MUJER","jugador" =>"miguel", "intentos" => 5 , "puntaje" => 11];
     $coleccionPartidas[10] = ["palabraWordix"=> "BARCO","jugador" =>"rodri", "intentos" => 5, "puntaje" => 11];
 
     return $coleccionPartidas;
